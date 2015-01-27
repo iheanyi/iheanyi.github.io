4e5ee675782a40e83ea3960a5eda978dc6a5bee9
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2015-01-27 14:48:31 -0500

    Build before deploying.

--- config.rb
+++ config.rb
@@ -63,6 +63,8 @@ end
 
 activate :deploy do |deploy|
   deploy.method = :git
+  deploy.build_before = true
+
   # Optional Settings
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   # deploy.branch   = 'custom-branch' # default: gh-pages
